a39dc72e8c6eb318decbef994455f881039ddcc1
ilikemysuitcase365 <karbaaa1234@gmail.com>
2025-04-21 17:05:21 +0800

    commoit

--- act1.py
+++ act1.py
@@ -1,9 +1,10 @@
 def shutdown():
-    input2 = input("Have you closed all apps?")
+    input1 = input("Have you closed all apps?")
     input2 = input("Have you logged off your account?")
-    if(input == "yes" and input2 == "yes"):
+    if(input1 == "yes" and input2 == "yes"):
         print("shutting down")
-    elif (input == "no" or input2 == "no"):
+    elif (input1  == "no" or input2 == "no"):
         print("almost shut down")
     else:
         print("sorry")
+shutdown()
